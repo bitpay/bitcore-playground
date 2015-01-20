66683a88f754fdff54d1085eed6b68b1198dfb70
Yemel Jardi <angel.jardi@gmail.com>
2015-01-20 02:45:36 -0300

    Progress on transaction page

--- app/app.js
+++ app/app.js
@@ -10,4 +10,15 @@ angular.module('playApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/units'});
-}]);
+}]).
+filter('btc', function() {
+  return function(satoshis) {
+    return bitcore.Unit.fromSatoshis(satoshis).toBTC() + ' BTC';
+  };
+}).
+controller('SideBar', function($scope){
+  $scope.setTestnet = function(value) {
+    var networks = bitcore.Networks;
+    networks.defaultNetwork = value ? networks.testnet : networks.livenet;
+  }
+});
--- app/transaction/transaction.js
+++ app/transaction/transaction.js
@@ -9,6 +9,43 @@ angular.module('playApp.transaction', ['ngRoute'])
   });
 }])
 
-.controller('TransactionCtrl', [function() {
-    console.log('Transaction controller OK');
-}]);
+.controller('TransactionCtrl', function($scope, $http) {
+    
+  $scope.utxoAddress = '1ERvrJ4zWVPapeLxuZQjRbL811Bif3YF3X';
+  $scope.utxos = [];
+
+  window.T = $scope.transaction = new bitcore.Transaction();
+
+  $scope.fetchUTXO = function(address) {
+    if (!bitcore.Address.isValid(address)) return; // mark as invalid
+    var client = new bitcore.transport.explorers.Insight();
+    client.getUnspentUtxos(address, onUTXOs);
+
+    function onUTXOs(err, utxos) {
+      if (err) throw err;
+
+      utxos = utxos.filter(function(u1) {
+        var repeated = $scope.utxos.filter(function(u2) {
+          return u2.txId === u1.txId && u1.outputIndex === u2.outputIndex;
+        });
+        return repeated.length === 0;
+      });
+
+      $scope.utxos = $scope.utxos.concat(utxos);
+      $scope.$apply();
+      console.log(utxos);
+    }
+  };
+
+  $scope.addUTXO = function(utxo) {
+    $scope.utxos = $scope.utxos.filter(function(u){
+      return u !== utxo;
+    });
+    $scope.transaction.from(utxo);
+  };
+
+  $scope.removeInput = function(input) {
+    
+  };
+
+});
