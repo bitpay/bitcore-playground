f68315fac0316e81c6570299480b6a1b6aa57949
Yemel Jardi <angel.jardi@gmail.com>
2015-01-27 13:18:10 -0300

    Add dynamic code examples to keys demo

--- app/keys/keys.js
+++ app/keys/keys.js
@@ -18,13 +18,14 @@ angular.module('playApp.keys', ['ngRoute'])
   $scope.newKey = function() {
     $scope.privateKey = new bitcore.PrivateKey();
     $scope.publicKey = $scope.privateKey.publicKey;
+    setExampleCode();
   };
-  
 
   $scope.privateUpdated = function(value) {
     if (bitcore.PrivateKey.isValid(value)) {
       $scope.privateKey = new bitcore.PrivateKey(value);
       $scope.publicKey = $scope.privateKey.publicKey;
+      setExampleCode($scope.privateKey);
     } else {
       // mark as invalid
     }
@@ -34,6 +35,7 @@ angular.module('playApp.keys', ['ngRoute'])
     if (bitcore.PublicKey.isValid(value)) {
       $scope.privateKey = '';
       $scope.publicKey = new bitcore.PublicKey(value);
+      setExampleCode(null, $scope.publicKey);
     } else {
       // mark as invalid
     }
@@ -47,12 +49,28 @@ angular.module('playApp.keys', ['ngRoute'])
     });
   };
 
+  function setExampleCode(privkey, pubkey) {
+    var template;
+    if (!privkey && !pubkey) {
+      template = "var privateKey = new bitcore.PrivateKey();\n";
+      template += "var publicKey = privateKey.publicKey;\n";
+    } else if (privkey) {
+      template = "var privateKey = new bitcore.PrivateKey('"+ privkey.toString() + "');\n";
+      template += "var publicKey = privateKey.publicKey;\n";
+    } else {
+      template = "var publicKey = new bitcore.PublicKey('"+ pubkey.toString()+ "');\n";
+    }
+    template += "var address = publicKey.toAddress();";
+    $scope.exampleCode = template;
+  };
+
   // Initialize
   if ($routeParams.data) {
     var data = JSON.parse($routeParams.data);
     bitcore.Networks.defaultNetwork = bitcore.Networks.get(data.network);
-    $scope.privateKey = new bitcore.PrivateKey(data.privateKey);
-    $scope.publicKey = new bitcore.PublicKey(data.publicKey);
+    $scope.privateKey = data.privateKey && new bitcore.PrivateKey(data.privateKey);
+    $scope.publicKey = data.publicKey && new bitcore.PublicKey(data.publicKey);
+    setExampleCode($scope.privateKey, $scope.publicKey);
   } else {
     $scope.newKey();
   }
