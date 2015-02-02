6a304e95bec62cf087ce95145b61566417781fab
Esteban Ordano <eordano@gmail.com>
2015-02-02 16:04:27 -0300

    Add unspent screen

---
+++ app/unspent/unspent.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('playApp.unspent', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/unspent', {
+    templateUrl: 'unspent/unspent.html',
+    controller: 'UnspentCtrl'
+  });
+}])
+
+.controller('UnspentCtrl', function($scope, $http) {
+
+  $scope.utxoAddress = '';
+    
+  $scope.utxos = [];
+
+  $scope.fetchUTXO = function(address) {
+    var client = new bitcore.transport.explorers.Insight();
+    if (!bitcore.Address.isValid(address)) return; // mark as invalid
+    client.getUnspentUtxos(address, onUTXOs);
+
+    function onUTXOs(err, utxos) {
+      if (err) throw err;
+
+      $scope.utxos = utxos;
+      $scope.$apply();
+      console.log(utxos);
+    }
+  };
+
+  $scope.serialize = function() {
+    return JSON.stringify({
+      address: $scope.utxoAddress,
+      utxos: $scope.utxos.map(function(utxo) { return utxo.toObject(); })
+    });
+  };
+
+  function setExampleCode() {
+    var template = "";
+    var i;
+
+    template += "var explorers = require('bitcore-explorers');\n";
+    template += "var insight = new explorers.Insight()\n";
+    template += "insight.getUnspentOutputs('" + $scope.utxoAddress + "', function(err, utxos) {\n";
+    template += "    // Check for errors or use the UTXOs...\n";
+    template += "});";
+
+    $scope.exampleCode = template;
+  }
+
+  function initialExample() {
+    var template = "";
+
+    template += "var explorers = require('bitcore-explorers');\n";
+    template += "var insight = new explorers.Insight()\n";
+    template += "insight.getUnspentOutputs('1BitcoinEaterAddressDontSendf59kuE', function(err, utxos) {\n";
+    template += "    // Check for errors or use the UTXOs...\n";
+    template += "});";
+
+    $scope.exampleCode = template;
+  }
+
+  initialExample();
+
+});
